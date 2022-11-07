2c30062cc2b1c084ca3d03d080a2e66fe716b8b6
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-11-07 10:22:01 +0100

    feat: exercise 1.6 complete
    - For those exercises that require long answers, akin to essay form, I
      opted to use a markdown (md) file for ease of writing

--- src/ch1/1.4.scm
+++
@@ -1,4 +0,0 @@
-(define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b))
-
-; We are conditionally determining the primitive procedure/operation to apply on operands `a` and `b`. The predicate here is whether `b` is an integer or a negative number. We are using a variation of a compound expression (special form) as the operator for the compound procedure
