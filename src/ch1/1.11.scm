dd8fb2d732083e607bb76b316b143c939d74d91f
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2023-02-05 03:44:14 -0600

    chore: update

---
+++ src/ch1/1.11.scm
@@ -0,0 +1,11 @@
+; Recursive
+(define (f n) (
+  (cond ((< n 3) n)
+        (else (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
+))
+
+; Iterative
+(define (f-iter n) (
+  (cond ((< n 3) n)
+        (else ()))
+))
