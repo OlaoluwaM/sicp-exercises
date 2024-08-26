29905ee16762fe306bf1df756a6e7112e48906c3
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2024-08-26 08:46:29 -0500

    feat: Completed ex1.35

---
+++ src/ch1/ex1.35/README.md
@@ -0,0 +1,9 @@
+# Exercise 1.35
+
+## Question
+
+Show that the golden ratio $φ$ (Section 1.2.2) is a fixed point of the transformation $x \mapsto 1 + \frac{1}{x}$ , and use this fact to compute $ϕ$ by means of the fixed-point procedure.
+
+## Solution
+
+[code](code.rkt)
---
+++ src/ch1/ex1.35/code.rkt
@@ -0,0 +1,33 @@
+#lang sicp
+
+(define (fixed-point f first-guess)
+  (define tolerance 0.00001)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((nextGuess (f guess)))
+      (if (close-enough? guess nextGuess)
+          nextGuess
+          (try nextGuess))))
+  (try first-guess))
+
+
+(define (round-to-places number places)
+  (let* ((factor (expt 10 places))
+         (rounded (* (round (* number factor)) (/ 1 factor))))
+    rounded))
+
+(define (fn x) (+ 1 (/ 1 x)))
+
+(define golden-ratio-by-fixed-point (fixed-point fn 1.0))
+(define dp 5)
+(define golden-ratio-by-fixed-point-rounded (round-to-places golden-ratio-by-fixed-point dp))
+
+(display "fixed-point golden ratio (rounded to 5 d.p): ")
+(display golden-ratio-by-fixed-point-rounded)
+(newline)
+
+(display "Proof: ")
+(display (= (round-to-places (fn golden-ratio-by-fixed-point) dp) golden-ratio-by-fixed-point-rounded))
+(newline)
